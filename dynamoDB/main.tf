2102aa05aed094c843cb4ddb8eaff489049ef2e1
lakhawat <slakhawat@oak9.io>
2022-06-21 14:42:07 -0500

    testing

---
+++ dynamoDB/main.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "dynamodb" {
+
+    server_side_encryption {
+      enabled = false
+      kms_key_arn= "arn:{redacted}"
+    }
+
+    aws_dynamodb_table.point_in_time_recovery.enabled = true
+    
+    
+
+}
